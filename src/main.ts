8eb5fa73fd5d0cb495e93ba9405a18748d436bd1
Janne Härkönen <janne.harkonen@reaktor.com>
2024-04-10 08:13:07 +0300

    barebones init

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import Handlebars from "handlebars";
+import { Libdoc } from "./testdata";
+import Translate from "./translate";
+
+function render(libdoc: Libdoc) {
+  Handlebars.registerHelper("t", function (key) {
+    return Translate.getInstance().getTranslation(key);
+  });
+
+  const template = document.getElementById("base-template")?.innerHTML;
+  const compiled_template = Handlebars.compile(template);
+
+  document.body.querySelector("#root").innerHTML = compiled_template(libdoc);
+}
+
+export default render;
